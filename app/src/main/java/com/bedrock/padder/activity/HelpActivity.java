9ae32832bc8507404a3df75e7f6a33b3794fb9b6
BedrockDev <dev.bedrock@gmail.com>
2017-06-10 04:09:17 +0900

    Working on #59

--- app/src/main/java/com/bedrock/padder/activity/HelpActivity.java
+++ app/src/main/java/com/bedrock/padder/activity/HelpActivity.java
@@ -1,89 +1,43 @@
 package com.bedrock.padder.activity;
 
-import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.bedrock.padder.R;
-import com.bedrock.padder.helper.AnimateHelper;
+import com.bedrock.padder.helper.ToolbarHelper;
 import com.bedrock.padder.helper.WindowHelper;
 
 public class HelpActivity extends AppCompatActivity {
 
-    private Activity activity = this;
-    private CollapsingToolbarLayout collapsingToolbarLayout = null;
+    private AppCompatActivity activity = this;
 
     private WindowHelper window = new WindowHelper();
-    private AnimateHelper anim = new AnimateHelper();
+    private ToolbarHelper toolbar = new ToolbarHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar_toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsedappbar);
-        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.expandedappbar);
-
-        window.setNavigationBar(R.color.transparent, activity);
-
-        View statusbar = findViewById(R.id.statusbar);
-        if (Build.VERSION.SDK_INT < 21) {
-            statusbar.setVisibility(View.GONE);
-        } else {
-            try {
-                statusbar.getLayoutParams().height = window.getStatusBarFromPrefs(activity);
-            } catch (NullPointerException e) {
-                Log.d("NullExp", e.getMessage());
-                statusbar.setVisibility(View.GONE);
-            }
-        }
-
-        window.setMarginRelativePX(R.id.layout_relative, 0, window.getStatusBarFromPrefs(activity), 0, 0, activity);
-        window.getView(R.id.layout_margin, activity).getLayoutParams().height = window.getNavigationBarFromPrefs(activity) + window.convertDPtoPX(10, activity);
-
-        enterAnim();
         setUi();
     }
 
     private void setUi() {
-        // status bar
-        window.getView(R.id.statusbar, activity).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        // set actionbar
+        toolbar.setActionBar(activity);
+        toolbar.setActionBarTitle(R.string.help_title);
+        toolbar.setActionBarPadding(activity);
+        toolbar.setActionBarColor(R.color.colorAccent, activity);
+        toolbar.setStatusBarTint(activity);
+        toolbar.setActionBarDisplayHomeAsUpIcon(R.drawable.ic_close_white);
 
-        // action bar
-        collapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorAccent));
-        collapsingToolbarLayout.setStatusBarScrimColor(getResources().getColor(R.color.colorAccent));
-        collapsingToolbarLayout.setTitle(window.getStringFromId("help_title", activity));
+        // transparent navigation bar
+        window.setNavigationBar(R.color.transparent, activity);
 
         // set taskDesc
-        window.setRecentColor(window.getStringFromId("help_title", activity), R.color.colorAccent, activity);
-    }
-
-    @Override
-    public void onBackPressed() {
-        anim.fadeOut(R.id.layout_text, 0, 200, activity);
-        anim.fadeOut(R.id.layout_detail, 0, 200, activity);
-        Handler delay = new Handler();
-        delay.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                HelpActivity.super.onBackPressed();
-            }
-        }, 200);
+        window.setRecentColor(R.string.help_title, R.color.colorAccent, activity);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -95,15 +49,9 @@ public class HelpActivity extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
 
         if (hasFocus) {
-            window.setGone(R.id.layout_placeholder, 0, activity);
             // reset taskDesc
             window.setRecentColor(window.getStringFromId("help_title", activity), R.color.colorAccent, activity);
         }
-    }
-
-    void enterAnim() {
-        anim.fadeIn(R.id.layout_text, 400, 200, "titleIn", activity);
-        anim.fadeIn(R.id.layout_help_layout, 500, 200, "helpIn", activity);
     }
 
     void pressBack() {
