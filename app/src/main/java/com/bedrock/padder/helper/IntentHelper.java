40932a4a6a7f9eee801e6b3764f0607098f31d69
BedrockDev <dev.bedrock@gmail.com>
2017-06-25 23:38:30 +0900

    Modified method variable names to java style

--- app/src/main/java/com/bedrock/padder/helper/IntentHelper.java
+++ app/src/main/java/com/bedrock/padder/helper/IntentHelper.java
@@ -53,7 +53,7 @@ public class IntentHelper {
         }
     }
 
-    public void intentWithExtra(final Activity activity, String name, final String extra_name, final String extra, int delay) {
+    public void intentWithExtra(final Activity activity, String name, final String extraName, final String extra, int delay) {
         final String classname = "com.bedrock.padder." + name;
         final Class<Object> classToLoad;
         try{
@@ -65,7 +65,7 @@ public class IntentHelper {
                 public void run() {
                     Log.i("IntentHelper", "intent");
                     Intent animActivity = new Intent(activity, classToLoad);
-                    animActivity.putExtra(extra_name, extra);
+                    animActivity.putExtra(extraName, extra);
                     activity.startActivity(animActivity);
                 }
             }, delay);
@@ -74,7 +74,7 @@ public class IntentHelper {
         }
     }
 
-    public void intentWithExtra(final Activity activity, String name, final String extra_name, final int extra, int delay) {
+    public void intentWithExtra(final Activity activity, String name, final String extraName, final int extra, int delay) {
         final String classname = "com.bedrock.padder." + name;
         final Class<Object> classToLoad;
         try{
@@ -86,7 +86,7 @@ public class IntentHelper {
                 public void run() {
                     Log.i("IntentHelper", "intent");
                     Intent animActivity = new Intent(activity, classToLoad);
-                    animActivity.putExtra(extra_name, extra);
+                    animActivity.putExtra(extraName, extra);
                     activity.startActivity(animActivity);
                 }
             }, delay);
@@ -136,8 +136,8 @@ public class IntentHelper {
         }
     }
 
-    public void intentSharedElement(final Activity activity, final String target_name, final int start_element_id, final String transition_name, int delay) {
-        final String classname = "com.bedrock.padder." + target_name;
+    public void intentSharedElement(final Activity activity, final String targetName, final int start_element_id, final String transitionName, int delay) {
+        final String classname = "com.bedrock.padder." + targetName;
         final Class<Object> classToLoad;
         try{
             classToLoad = (Class<Object>)Class.forName(classname);
@@ -153,11 +153,11 @@ public class IntentHelper {
                         ActivityOptionsCompat options =
                                 ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
                                         view,           // The view which starts the transition
-                                        transition_name // The transitionName of the view we’re transitioning to
+                                        transitionName // The transitionName of the view we’re transitioning to
                                 );
                         ActivityCompat.startActivity(activity, intent, options.toBundle());
                     } else {
-                        intent(activity, target_name);
+                        intent(activity, targetName);
                     }
                 }
             }, delay);
@@ -166,8 +166,8 @@ public class IntentHelper {
         }
     }
 
-    public void intentSharedElementWithExtra(final Activity activity, final String target_name, final int start_element_id, final String transition_name, final String extra_name, final int extra, int delay) {
-        final String classname = "com.bedrock.padder." + target_name;
+    public void intentSharedElementWithExtra(final Activity activity, final String targetName, final int start_element_id, final String transitionName, final String extraName, final int extra, int delay) {
+        final String classname = "com.bedrock.padder." + targetName;
         final Class<Object> classToLoad;
         try{
             classToLoad = (Class<Object>)Class.forName(classname);
@@ -178,17 +178,17 @@ public class IntentHelper {
                 public void run() {
                     if(Build.VERSION.SDK_INT >= 21) {
                         Intent intent = new Intent(activity, classToLoad);
-                        intent.putExtra(extra_name, extra);
+                        intent.putExtra(extraName, extra);
                         View view = (View) activity.findViewById(start_element_id);
 
                         ActivityOptionsCompat options =
                                 ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
                                         view,           // The view which starts the transition
-                                        transition_name // The transitionName of the view we’re transitioning to
+                                        transitionName // The transitionName of the view we’re transitioning to
                                 );
                         ActivityCompat.startActivity(activity, intent, options.toBundle());
                     } else {
-                        intentWithExtra(activity, target_name, extra_name, extra, 0);
+                        intentWithExtra(activity, targetName, extraName, extra, 0);
                     }
                 }
             }, delay);
@@ -197,8 +197,8 @@ public class IntentHelper {
         }
     }
 
-    public void intentSharedElementWithExtra(final Activity activity, final String target_name, final int start_element_id, final String transition_name, final String extra_name, final String extra, int delay) {
-        final String classname = "com.bedrock.padder." + target_name;
+    public void intentSharedElementWithExtra(final Activity activity, final String targetName, final int start_element_id, final String transitionName, final String extraName, final String extra, int delay) {
+        final String classname = "com.bedrock.padder." + targetName;
         final Class<Object> classToLoad;
         try{
             classToLoad = (Class<Object>)Class.forName(classname);
@@ -209,17 +209,17 @@ public class IntentHelper {
                 public void run() {
                     if(Build.VERSION.SDK_INT >= 21) {
                         Intent intent = new Intent(activity, classToLoad);
-                        intent.putExtra(extra_name, extra);
+                        intent.putExtra(extraName, extra);
                         View view = (View) activity.findViewById(start_element_id);
 
                         ActivityOptionsCompat options =
                                 ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
                                         view,           // The view which starts the transition
-                                        transition_name // The transitionName of the view we’re transitioning to
+                                        transitionName // The transitionName of the view we’re transitioning to
                                 );
                         ActivityCompat.startActivity(activity, intent, options.toBundle());
                     } else {
-                        intentWithExtra(activity, target_name, extra_name, extra, 0);
+                        intentWithExtra(activity, targetName, extraName, extra, 0);
                     }
                 }
             }, delay);
@@ -228,8 +228,8 @@ public class IntentHelper {
         }
     }
 
-    public void intentSharedElementWithExtra(final Activity activity, final String target_name, final int start_element_id, final String transition_name, final String extra_name, final String extra, int delay, final View v) {
-        final String classname = "com.bedrock.padder." + target_name;
+    public void intentSharedElementWithExtra(final Activity activity, final String targetName, final int start_element_id, final String transitionName, final String extraName, final String extra, int delay, final View v) {
+        final String classname = "com.bedrock.padder." + targetName;
         final Class<Object> classToLoad;
         try{
             classToLoad = (Class<Object>)Class.forName(classname);
@@ -240,17 +240,17 @@ public class IntentHelper {
                 public void run() {
                     if(Build.VERSION.SDK_INT >= 21) {
                         Intent intent = new Intent(activity, classToLoad);
-                        intent.putExtra(extra_name, extra);
+                        intent.putExtra(extraName, extra);
                         View view = v.findViewById(start_element_id);
 
                         ActivityOptionsCompat options =
                                 ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
                                         view,           // The view which starts the transition
-                                        transition_name // The transitionName of the view we’re transitioning to
+                                        transitionName // The transitionName of the view we’re transitioning to
                                 );
                         ActivityCompat.startActivity(activity, intent, options.toBundle());
                     } else {
-                        intentWithExtra(activity, target_name, extra_name, extra, 0);
+                        intentWithExtra(activity, targetName, extraName, extra, 0);
                     }
                 }
             }, delay);
@@ -283,7 +283,7 @@ public class IntentHelper {
         }
     }
 
-    public void intentFlagWithExtra(final Activity activity, String name, final String extra_name, final String extra, int delay) {
+    public void intentFlagWithExtra(final Activity activity, String name, final String extraName, final String extra, int delay) {
         final String classname = "com.bedrock.padder." + name;
         final Class<Object> classToLoad;
         try{
@@ -295,7 +295,7 @@ public class IntentHelper {
                 public void run() {
                     Log.i("IntentHelper", "intentFlag");
                     Intent animActivity = new Intent(activity, classToLoad);
-                    animActivity.putExtra(extra_name, extra);
+                    animActivity.putExtra(extraName, extra);
                     animActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     animActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     activity.startActivity(animActivity);
@@ -382,19 +382,19 @@ public class IntentHelper {
         }, delay);
     }
 
-    public void intentEmail(final Activity activity, String email_target, String subject, String subject_desc, String text, String text_desc, final String hint, int delay) {
+    public void intentEmail(final Activity activity, String email_target, String subject, String subjectDesc, String text, String textDesc, final String hint, int delay) {
         String br = System.getProperty("line.separator");
         String info;
-        if(text_desc == null || text_desc.equals("")) {
+        if(textDesc == null || textDesc.equals("")) {
             info = br + br + br + br + "Client : Android [" + BuildConfig.VERSION_NAME + "]";
         } else {
-            info = br + br + br + br + "Link : " + text_desc + br + br + "Client : Android [" + BuildConfig.VERSION_NAME + "]";
+            info = br + br + br + br + "Link : " + textDesc + br + br + "Client : Android [" + BuildConfig.VERSION_NAME + "]";
         }
 
         final Intent email = new Intent(Intent.ACTION_SEND);
         email.setType("message/rfc822");
         email.putExtra(Intent.EXTRA_EMAIL  , new String[]{email_target});
-        email.putExtra(Intent.EXTRA_SUBJECT, subject_desc + " : " + subject);
+        email.putExtra(Intent.EXTRA_SUBJECT, subjectDesc + " : " + subject);
         email.putExtra(Intent.EXTRA_TEXT   , text + info);
 
         Handler emailDelay = new Handler();
