e1eb9720d3b12cae24cb54c805efce07fd3693b8
BedrockDev <dev.bedrock@gmail.com>
2017-07-24 01:44:30 +0900

    Modified Generic classes to Object class, needs fix

--- app/src/main/java/com/bedrock/padder/model/app/SettingItem.java
+++ app/src/main/java/com/bedrock/padder/model/app/SettingItem.java
@@ -1,27 +1,52 @@
 package com.bedrock.padder.model.app;
 
 import android.content.SharedPreferences;
+import android.util.Log;
 
-public class SettingItem<T> {
+public class SettingItem {
 
-    private T item;
+    private Object item;
 
-    private final Class<T> type;
+    private String TAG = "SettingItem";
 
-    public SettingItem(T item, Class<T> type) {
+    public SettingItem(Object item) {
         this.item = item;
-        this.type = type;
     }
 
-    public void setItem(SharedPreferences prefs) {
+    public void setItem(SharedPreferences prefs, String key, Object value) {
         if (prefs != null) {
-
+            if (item instanceof Boolean) {
+                // boolean variable
+                item = value;
+                prefs.edit().putBoolean(key, (boolean) value).apply();
+            } else if (item instanceof Float) {
+                // float variable
+                item = value;
+                prefs.edit().putFloat(key, (float) value).apply();
+            } else if (item instanceof String) {
+                // string variable
+                item = value;
+                prefs.edit().putString(key, (String) value).apply();
+            }
         } else {
-
+            Log.d(TAG, "Prefs null");
         }
     }
 
-    public Class<T> getType() {
-        return this.type;
+    public Object getItem() {
+        return item;
+    }
+
+    public void fetchItem(SharedPreferences prefs, String key, Object defValue) {
+        if (item instanceof Boolean) {
+            // boolean variable
+            item = prefs.getBoolean(key, (boolean) defValue);
+        } else if (item instanceof Float) {
+            // float variable
+            item = prefs.getFloat(key, (float) defValue);
+        } else if (item instanceof String) {
+            // string variable
+            item = prefs.getString(key, (String) defValue);
+        }
     }
 }
